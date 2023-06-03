c105cf0202b4e2c89809913a2e2e505206cf9704
dodiyah330 <dodiyah330@gmail.com>
2023-06-03 17:15:39 +0530

    Designed

---
+++ src/content_option.js
@@ -0,0 +1,185 @@
+const logotext = "HITESH";
+const meta = {
+    title: "Hitesh Dodiya",
+    description: "I’m Hitesh Dodiya MERN stack devloper,currently working in Rajkot",
+};
+
+const introdata = {
+    title: "I’m Hitesh Dodiya",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web apps",
+    },
+    description: "As a MERN stack developer, I specialize in building dynamic and responsive web applications using MongoDB, Express, React, and Node.js. With experience in both front-end and back-end development, I thrive on creating efficient and scalable solutions to complex problems. My expertise in JavaScript, HTML, CSS, and various libraries and frameworks enables me to deliver high-quality products that meet the needs of clients and users.",
+    your_img_url: "https://images.unsplash.com/photo-151                  j4790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "As a MERN stack developer, I specialize in building dynamic and responsive web applications using MongoDB, Express, React, and Node.js. With experience in both front-end and back-end development, I thrive on creating efficient and scalable solutions to complex problems. My expertise in JavaScript, HTML, CSS, and various libraries and frameworks enables me to deliver high-quality products that meet the needs of clients and users.",
+};
+const worktimeline = [{
+    jobtitle: "Mern stack developer at Cypherox technology",
+    where: "Rajkot, Gujarat, India",
+    date: "06/2022 - 07/2023",
+},
+{
+    jobtitle: "Mern stack developer- trainee at Cypherox technology",
+    where: "Rajkot, Gujarat, India",
+    date: "05/2022 - 06/2022",
+},
+{
+    jobtitle: "Mern stack developer- trainee at Besticoder",
+    where: "Keshod, Gujarat, India",
+    date: "02/2021-02/2022",
+},
+];
+
+const skills = [{
+    name: "HTML",
+    value: 80,
+},
+{
+    name: "CSS",
+    value: 80,
+},
+{
+    name: "Javascript",
+    value: 85,
+},
+{
+    name: "Bootstrap",
+    value: 75,
+},
+{
+    name: "ReactJS",
+    value: 90,
+},
+{
+    name: "NodeJS",
+    value: 60,
+},
+{
+    name: "ExpressJS",
+    value: 60,
+},
+{
+    name: "MondoDB",
+    value: 65,
+},
+];
+
+const services = [{
+    title: "E-Resume",
+    description: `As a MERN stack developer, I worked on a project called E-Resume, which aimed to provide users with an easy way to create and download resumes in just a few minutes.
+    The application featured essential functionalities such as user registration and login, which
+    were secured with authentication to ensure the privacy of user data. Users could make
+    multiple resumes and edit or delete them as per their requirements.
+     To make the process even more convenient for users, the application offered demo resumes
+    in various fields, making it easier to understand and create resumes based on the user's
+    career interests.
+     Finally, users could download their resumes in PDF format, making it easier to share their
+    resumes with potential employers.
+     The project was developed using ReactJs for the frontend, ExpressJs for the backend, and
+    MySQL for data management, ensuring a scalable and efficient application that met the
+    needs of the users.`,
+},
+{
+    title: "Chat App",
+    description: "Introducing our cutting-edge chat app built with ReactJS, NodeJS, ExpressJS, and Socket.IO! With a sleek and intuitive user interface, this app offers seamless communication, real-time messaging, and an immersive chatting experience. Stay connected with friends, family, and colleagues like never before. Experience the perfect blend of advanced technology and captivating design, all in one powerful chat application.",
+}
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "dodiyah330@gmail.com",
+    YOUR_FONE: "9725797443",
+    description: "Get in touch with me today. We'd love to hear from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_woiw6zs",
+    YOUR_TEMPLATE_ID: "template_id8ozl4",
+    YOUR_USER_ID: "vRrZPICUjDEsamWOt",
+};
+
+const socialprofils = {
+    github: "https://github.com/dodiyah330",
+    facebook: "https://www.facebook.com/profile.php?id=100064943934105",
+    linkedin: "https://www.linkedin.com/in/hitesh-dodiya-4061171b2/",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
